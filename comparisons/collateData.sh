ddb911adf3e9dde53fe735ccf0093f580c23cfd6
iain-logan <iainlogan94@gmail.com>
2016-03-24 02:37:27 +0000

    Fixed up naming a bit in collated data

--- comparisons/collateData.sh
+++ comparisons/collateData.sh
@@ -6,19 +6,22 @@ sing="./sing/Vec.hs"
 agda="./agda/Vec.agda"
 
 sources=("$she" "$sing" "$hask" "$agda")
+names=("she" "singleton" "haskell" "agda")
 
 rm results.csv
 
 touch results.csv
 
-echo "Source,Lines,Characters" >> results.csv
+echo "# Source,Lines,Characters" >> results.csv
 
-for s in ${sources[@]}; do
-    printf "%s" "$s," >> results.csv
-    lines=`cat "$s" | wc -l`
-    chars=`cat "$s" | wc -m`
+for ((i=0;i<${#sources[@]};++i)); do
+    printf "%s" "${names[i]}," >> results.csv
+    lines=`cat "${sources[i]}" | wc -l`
+    chars=`cat "${sources[i]}" | wc -m`
     printf "%s" "$lines," >> results.csv
     echo "$chars" >> results.csv
 done
 
 exit 0
+
+${array[i]}
